e913bfade81e4d544ad840b6e201cf1d54466a03
wikkyfan <whmg1234@hotmail.com>
2023-02-27 11:44:04 +0800

    feat:add basealgo

---
+++ src/basealg/ibasealgorithm.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <condition_variable>
+#include <memory>
+#include <string>
+#include <list>
+#include <stdint.h>
+#include <thread>
+#include <chrono>
+#include <future>
+#include <functional>
+#include <intsafe.h>
+#include "common.h"
+
+namespace basealgo
+{
+class IBaseAlg
+{
+public:
+    virtual ~IBaseAlg() {}
+    virtual bool initAlgoparam(std::string&) = 0;
+    virtual int doing(smartmore::SingleMat&) = 0;
+};
+
+typedef IBaseAlg* (__stdcall* pExportALG)();
+typedef void (__stdcall* pDeleteALG)(IBaseAlg*);
+}  // namespace cameramanager
+
