89a2b13137c4d2be94b27fc9a9ee71edae5fdebe
wikkyfan <whmg1234@hotmail.com>
2023-07-03 12:21:38 +0800

    feat:update

--- cmake/fetch/spdlog.cmake
+++ cmake/fetch/spdlog.cmake
@@ -1,9 +1,16 @@
 include(FetchContent)
 
 FetchContent_Declare(spdlog
-    GIT_REPOSITORY https://github.com/gabime/spdlog
-    GIT_TAG v1.9.2
-    GIT_SHALLOW ON
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    GIT_TAG v1.11.0
 )
 
 FetchContent_MakeAvailable(spdlog)
+#
+#set(spdlog_BUILD_TESTS OFF) # 设置好变量用于关掉AAA项目的测试
+#
+#FetchContent_GetProperties(spdlog)
+#if(NOT spdlog_POPULATED) 
+#  FetchContent_Populate(spdlog)
+#  add_subdirectory(${spdlog_SOURCE_DIR} ${spdlog_BINARY_DIR})
+#endif ()
--- cmake/fetch/wikky_dll.cmake
+++ cmake/fetch/wikky_dll.cmake
@@ -1,26 +1,11 @@
 include(FetchContent)
 if(NOT WIKKYDLL_Found)
 
-# if(NOT DEFINED WIKKYDLL_URL)
-# if(WIN32)
-#      set(WIKKYDLL_URL "git@github.com:wikky250/wikky_dll.git")
-# else()
-#     message(FATEL_ERROR "Need Opencv4.5-linux package.")
-# endif()
-# endif()
+FetchContent_Declare(
+  WIKKYDLL
+  GIT_REPOSITORY    git@github.com:wikky250/wikky_dll.git
+  GIT_TAG           master
+)
 
-# FetchContent_Declare(WIKKYDLL
-#     URL ${OpenCV_URL}
-# )
-message(STATUS "Fetching WIKKYDLL : ${OpenCV_URL}")
 FetchContent_MakeAvailable(WIKKYDLL)
-message(STATUS "Fetching done")
-
-FetchContent_GetProperties(WIKKYDLL SOURCE_DIR OpenCV_SOURCE_DIR)
-message(STATUS "WIKKYDLL_DIR src dir : ${OpenCV_SOURCE_DIR}")
-# get OpenCV_DIR
-if(WIN32)
-    set(WIKKYDLL_DIR ${OpenCV_SOURCE_DIR}/x64/vc15/lib)
-endif()
-
 endif()
