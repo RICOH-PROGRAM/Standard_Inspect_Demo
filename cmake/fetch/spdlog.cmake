9048fe1338c56304207ee7630ace91f27d5484b3
wikkyfan <whmg1234@hotmail.com>
2023-07-17 11:57:45 +0800

    feat: modify cmake

--- cmake/fetch/spdlog.cmake
+++ cmake/fetch/spdlog.cmake
@@ -13,11 +13,3 @@ FetchContent_Declare(spdlog
 endif()
 
 FetchContent_MakeAvailable(spdlog)
-#
-#set(spdlog_BUILD_TESTS OFF) # 设置好变量用于关掉AAA项目的测试
-#
-#FetchContent_GetProperties(spdlog)
-#if(NOT spdlog_POPULATED) 
-#  FetchContent_Populate(spdlog)
-#  add_subdirectory(${spdlog_SOURCE_DIR} ${spdlog_BINARY_DIR})
-#endif ()
